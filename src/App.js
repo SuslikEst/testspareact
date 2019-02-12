3005d1b1613a5754ebe6704660fcb6fac6e1fc54
zsv <zsv@linuxwizard.ru>
2019-02-12 09:04:55 +0300

    fix

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const App  = () => {
+
+    return (
+      <div className="App">
+        Hello world!
+      </div>
+    );
+
+}
+
+export default App;
